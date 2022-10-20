b2cc89be5936c313c3949417f2161a0c5587c78c
giou55 <giourmetakis00@gmail.com>
2022-10-20 15:22:58 +0300

    changes

--- SportsStore/Program.cs
+++ SportsStore/Program.cs
@@ -11,12 +11,11 @@ builder.Services.AddDbContext<StoreDbContext>(opts => {
 });
 
 builder.Services.AddScoped<IStoreRepository, EFStoreRepository>();
-
 builder.Services.AddRazorPages();
-
 builder.Services.AddDistributedMemoryCache();
-
 builder.Services.AddSession();
+builder.Services.AddScoped<Cart>(sp => SessionCart.GetCart(sp));
+builder.Services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
 
 var app = builder.Build();
 
